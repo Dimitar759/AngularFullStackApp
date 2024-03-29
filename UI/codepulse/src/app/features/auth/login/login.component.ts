765018fc5a306b59584fb186a91d81fdb5b5b597
Dimitar759 <dimitarnikolov759@gmail.com>
2024-03-29 11:51:40 +0100

    Login functionality added

--- UI/codepulse/src/app/features/auth/login/login.component.ts
+++ UI/codepulse/src/app/features/auth/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { LoginRequest } from '../models/login-request.model';
+import { AuthService } from '../services/services.service';
+import { log } from 'console';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +13,9 @@ import { LoginRequest } from '../models/login-request.model';
 export class LoginComponent {
   model: LoginRequest;
 
-  constructor(){
+  constructor(private authService: AuthService,
+    private cookieService: CookieService,
+    private router: Router){
     this.model = {
       email: '',
       password: ''
@@ -18,7 +24,25 @@ export class LoginComponent {
   }
 
   onFormSubmit(): void {
-    console.log(this.model);
+    this.authService.login(this.model)
+    .subscribe({
+      next: (response) => {
+        //set auth cookie
+        this.cookieService.set('Authorization', `Bearer ${response.token}`,
+        undefined, '/', undefined, true, 'Strict');
+
+        //set the user
+
+        this.authService.setUser({
+          email: response.email,
+          roles: response.roles
+        });
+
+        //Redirect back to Home
+        this.router.navigateByUrl('/');
+        
+      }
+    })
     
   }
 }
